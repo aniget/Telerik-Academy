4c1d2a2ee60a9be9ef5c70536b62af430f5d616a
Unknown <ahristoff@abv.bg>
2017-11-24 12:18:30 +0200

    Adding MDA

---
+++ MDA_practice/01. Fill The Matrix/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fill_The_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nr = int.Parse(Console.ReadLine());
+            var nc = nr;
+            int r = 0;
+            int c = 0;
+
+            string pattern = Console.ReadLine();
+            int[,] matrix = new int[nr, nc];
+            int counter;
+
+            //1 5 9 13
+            //2 6 10 14
+            //3 7 11 15
+            //4 8 12 16
+            //
+            if (pattern == "a")
+            {
+                counter = 1;
+                for (c = 0; c < matrix.GetLength(1); c++)
+
+                {
+                    for (r = 0; r < matrix.GetLength(0); r++)
+                    {
+                        matrix[r, c] = counter;
+                        counter++;
+                    }
+                }
+            }
+
+            //1 8 9 16
+            //2 7 10 15
+            //3 6 11 14
+            //4 5 12 13
+
+            if (pattern == "b")
+            {
+                counter = 1;
+                for (c = 0; c < nr; c++)
+                {
+                    if (c % 2 == 0)
+                    {
+                        for (r = 0; r < nr; r++)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        for (r = nr - 1; r > -1; r--)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                    }
+                }
+
+            }
+
+            //7 11 14 16
+            //4 8 12 15
+            //2 5 9 13
+            //1 3 6 10
+
+            if (pattern == "c")
+            {
+                counter = 1;
+                int max_j = 1;
+                //i - the number of iterations
+                for (int i = 1; i < 2 * nr; i++)
+                {
+                    //j - the number of elements per iteration
+                    r = nr - i;
+                    if (i > nr)
+                    {
+                        max_j = 2 * nr - i;
+                        r = 0;
+                        c = i - nr;
+                    }
+                    else
+                    {
+                        max_j = i;
+                        r = nr - i;
+                        c = 0;
+                    }
+                    for (int j = 1; j < max_j + 1; j++)
+                    {
+                        matrix[r, c] = counter;
+                        counter++;
+                        c++;
+                        r++;
+                    }
+                }
+            }
+
+            //1 12 11 10
+            //2 13 16  9
+            //3 14 15  8
+            //4  5  6  7
+            if (pattern == "d")
+            {
+                counter = 1;
+                //change direction in a cycle
+                //down, right, up, left
+                int count_circle = 0;
+
+                do
+                {
+                    // DOWN
+                    c = count_circle;
+                    for (r = count_circle; r < nr; r++)
+                    {
+                        if (matrix[r, c] == 0)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // RIGTH
+                    r = nr - count_circle - 1;
+                    for (c = count_circle + 1; c < nr; c++)
+                    {
+                        if (matrix[r, c] == 0)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // UP
+                    c = nr - count_circle - 1;
+                    for (r = nr - 2 - count_circle; r > -1 + count_circle; r--)
+                    {
+                        if (matrix[r, c] == 0)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    // LEFT
+                    r = count_circle;
+                    for (c = nr - 2 - count_circle; c > -1 + count_circle; c--)
+                    {
+                        if (matrix[r, c] == 0)
+                        {
+                            matrix[r, c] = counter;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    count_circle++;
+                } while (nr * nr > counter - 1);
+            }
+
+
+            PrintMatrix(matrix);
+
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    //Console.Write(matrix[i, j].ToString().PadLeft(3) + " "); //PadLeft
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+
+}
---
+++ MDA_practice/03. Sequience in Numbers/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sequience_in_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] result = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+            int? count;
+            int? max = 1; //max sum
+            int? maxR = 1;
+            int? maxC = 1;
+            int? maxD1 = 1;
+            int? maxD2 = 1;
+            int rows = result[0];
+            int cols = result[1];
+            string[,] matrix = new string[rows, cols];
+
+            string line = "";
+            string[] lineArr = new string[cols];
+
+            //read console input into array
+            for (int row = 0; row < rows; row++)
+            {
+                line = Console.ReadLine();
+                lineArr = line.Split(' ').Select(x => x).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = lineArr[col];
+                }
+            }
+
+            //read and analyze array
+
+
+            //read ROWS
+            for (int r = 0; r < rows; r++)
+            {
+                count = 1;
+                for (int c = 0; c < cols - 1; c++)
+                {
+                    if (matrix[r, c] == matrix[r, c + 1])
+                    {
+                        count++;
+                        if (count > maxR)
+                        {
+                            maxR = count;
+                        }
+                    }
+                    else
+                    {
+                        count = 1;
+                    }
+
+                }
+            }
+
+            //read COLUMNS
+
+            for (int c = 0; c < cols; c++)
+            {
+                count = 1;
+                for (int r = 0; r < rows - 1; r++)
+                {
+                    if (matrix[r, c] == matrix[r + 1, c])
+                    {
+                        count++;
+                        if (count > maxC)
+                        {
+                            maxC = count;
+                        }
+                    }
+                    else
+                    {
+                        count = 1;
+                    }
+
+                }
+            }
+
+            //read DIAGONALS 1 (1 of 2)
+
+            //loop through diagonals
+            for (int r = 0; r < rows - 1; r++)
+            {
+                int c = 0;
+                count = 1;
+                //loop through diagonal elements
+                for (int r1 = r; r1 < rows - 1; r1++)
+                {
+                    if (c < cols - 1)
+                    {
+                        if (matrix[r1, c] == matrix[r1 + 1, c + 1])
+                        {
+                            count++;
+                            if (count > maxD1)
+                            {
+                                maxD1 = count;
+                            }
+                        }
+                        else
+                        {
+                            count = 1;
+                        }
+                        c++;
+                    }
+                }
+            }
+
+            //read DIAGONALS 1 (2 of 2)
+
+            //loop through diagonals
+            for (int c = 0; c < rows - 1; c++)
+            {
+                int c1 = c;
+                count = 1;
+                //loop through diagonal elements
+                for (int r = 0; r < rows - 1; r++)
+                {
+                    if (c1 < cols - 1)
+                    {
+                        if (matrix[r, c1] == matrix[r + 1, c1 + 1])
+                        {
+                            count++;
+                            if (count > maxD1)
+                            {
+                                maxD1 = count;
+                            }
+                        }
+                        else
+                        {
+                            count = 1;
+                        }
+                        c1++;
+                    }
+
+                }
+            }
+
+
+            //read DIAGONALS 2 (1 of 2)
+
+            //loop through diagonals
+            for (int r = 0; r < rows - 1; r++)
+            {
+                int c = cols - 1;
+                count = 1;
+                //loop through diagonal elements
+                for (int r1 = r; r1 < rows - 1; r1++)
+                {
+                    if (c > 0)
+                    {
+                        if (matrix[r1, c] == matrix[r1 + 1, c - 1])
+                        {
+                            count++;
+                            if (count > maxD2)
+                            {
+                                maxD2 = count;
+                            }
+                        }
+                        else
+                        {
+                            count = 1;
+                        }
+                        c--;
+
+                    }
+                }
+            }
+
+            //read DIAGONALS 2 (2 of 2)
+            //loop through diagonals
+            for (int c = cols - 1; c > 0; c--)
+            {
+                count = 1;
+                int c1 = c;
+                //loop through diagonal elements
+                for (int r1 = 0; r1 < rows - 1; r1++)
+                {
+                    if (c1 > 0)
+                    {
+                        if (matrix[r1, c1] == matrix[r1 + 1, c1 - 1])
+                        {
+                            count++;
+                            if (count > maxD2)
+                            {
+                                maxD2 = count;
+                            }
+                        }
+                        else
+                        {
+                            count = 1;
+                        }
+                        c1--;
+                    }
+
+                }
+            }
+
+            max = new[] { maxR, maxC, maxD1, maxD2 }.Max();
+            Console.WriteLine(max);
+        }
+    }
+}
