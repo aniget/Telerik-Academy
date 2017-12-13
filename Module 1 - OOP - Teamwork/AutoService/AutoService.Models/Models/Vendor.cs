311eb47abd3fb066edfb15f7ce38a8683d185756
Alex Parvanov <aparvanov@abv.bg>
2017-12-13 23:49:38 +0200

    added new classes

---
+++ Module 1 - OOP - Teamwork/AutoService/AutoService.Models/Models/Vendor.cs
@@ -0,0 +1,20 @@
+﻿using AutoService.Models.Contracts;
+
+namespace AutoService.Models.Models
+{
+    public class Vendor : Company, IVendor
+    {
+        public Vendor(string name, string address, string uniqueNumber, decimal creditLimit) : base(name, address, uniqueNumber, creditLimit)
+        {
+        }
+
+        public Vendor(string name, string address, string uniqueNumber, decimal creditLimit, string taxNumber) : base(name, address, uniqueNumber, creditLimit, taxNumber)
+        {
+        }
+
+        public void PayInvoices()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
